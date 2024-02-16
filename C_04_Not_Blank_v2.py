5bb18920005b041cd66cb2da6eb956d0c6af2a21
JackPringle <101750172+JackPringle@users.noreply.github.com>
2024-02-16 13:30:39 +1300

    changed name to 04

---
+++ C_04_Not_Blank_v2.py
@@ -0,0 +1,24 @@
+# Functions...
+
+# Checks that the user response is not blank
+def not_blank(question):
+    while True:
+        response = input(question).strip()
+
+        # If the response is blank after stripping whitespace, output error
+        if response == "":
+            print("Sorry this can't be blank. Please try again")
+
+        else:
+            return response
+
+
+# Main Routine...
+
+while True:
+    # Ask user what their name is, call not blank function
+    name = not_blank("Enter your name (or 'xxx' to quit): ")
+
+    # If they have chosen to quit, output the name they chose
+    if name != "xxx":
+        print(f"Your name is {name}")
