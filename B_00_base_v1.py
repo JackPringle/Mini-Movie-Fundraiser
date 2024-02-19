f4d3d922194f317e2bdd19013601a615ca1b6ea7
JackPringle <101750172+JackPringle@users.noreply.github.com>
2024-02-19 14:25:14 +1300

    Added age checker

--- B_00_base_v1.py
+++ B_00_base_v1.py
@@ -1,15 +1,96 @@
+# Functions...
+
+# Checks user has entered yes / no to a question
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+
+        # If the response is yes or y, return yes
+        if response == "yes" or response == "y":
+            return "yes"
+
+        # If the response is no or n, return no
+        elif response == "no" or response == "n":
+            return "no"
+
+        # If exit code is entered, break from loop
+        elif response == "xxx":
+            print(" - You have chosen to exit -")
+            break
+
+        # Other responses will trigger a helpful statement
+        else:
+            print("Please enter yes or no")
+
+
+# Checks that the user response is not blank
+def not_blank(question):
+    while True:
+        response = input(question).strip()
+
+        # If the response is blank after stripping whitespace, output error
+        if response == "":
+            print("Sorry this can't be blank. Please try again")
+        else:
+            return response
+
+
+# Checks user enters an integer to a given question
+def num_check(question):
+
+    while True:
+
+        try:
+
+            # check response for an integer
+            response = int(input(question))
+            return response
+
+        # If not an integer, output error
+        except ValueError:
+            print("Please enter an integer")
+
+
 # Main Routine...
 
 # Set maximum number of tickets below
 MAX_TICKETS = 3
+tickets_sold = 0
+
+# Ask the user if they want to see the instructions
+want_instructions = yes_no("Do you want to read the instructions? ")
+
+# If they say yes, show the instructions
+if want_instructions == "yes" or want_instructions == "y":
+    print("Show Instructions...")
+
+# If they say no, continue program
+elif want_instructions == "no" or want_instructions == "n":
+    print()
 
 # Loop to sell tickets
-tickets_sold = 0
 while tickets_sold < MAX_TICKETS:
-    name = input("Please enter your name or 'xxx' to quit: ")
+    name = not_blank("Please enter your name or 'xxx' to quit: ")
 
     if name == "xxx":
         break
+    else:
+        print(f"Your name is {name}")
+
+    # Ask for age, call number checker
+    age = num_check("Age: ")
+
+    # Accept user age if it is >= 12 or <= 120
+    if 12 <= age <= 120:
+        pass
+
+    # Check if the user is too young
+    elif age < 12:
+        print("Sorry you must be at least 12 to watch this movie!")
+        continue
+    else:
+        print("?? That looks like a typo, please try again!")
+        continue
 
     tickets_sold += 1
 
